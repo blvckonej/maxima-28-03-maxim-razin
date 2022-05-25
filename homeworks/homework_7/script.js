7ce1fbbcd831d1428392268944c44760ef99e5fb
Maxim Razin <blvckonej@gmail.com>
2022-05-26 00:27:08 +0300

    added homework 7 and calc script

---
+++ homeworks/homework_7/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const input1 = document.getElementById('value1');
+const input2 = document.getElementById('value2');
+const signInput = document.getElementById('signs');
+const button = document.querySelector('button');
+const result = document.getElementById('result');
+
+button.addEventListener('click', () => {
+    const value1 = input1.value;
+    const value2 = input2.value;
+    const signs = signInput.value;
+
+    if (signs === '+') {
+        const result1 = Number(value1) + Number(value2);
+        result.textContent = result1;
+    } else if (signs === '') {
+        alert("Введите знак");
+    } else if (value1 === '') {
+        alert("Введите первое число");
+    } else if (value2 === '') {
+        alert("Введите второе число");
+    } else if (signs === '-') {
+        const result1 = Number(value1) - Number(value2);
+        result.textContent = result1;
+    } else if (signs === '*') {
+        const result1 = Number(value1) * Number(value2);
+        result.textContent = result1;
+    } else if (signs === '/') {
+        const result1 = Number(value1) / Number(value2);
+        result.textContent = result1;
+    }
+})
