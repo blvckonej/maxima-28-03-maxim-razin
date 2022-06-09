9b0747e87d1829b1ef3c8caccb391c59e3c05096
Maxim Razin <blvckonej@gmail.com>
2022-06-09 16:02:04 +0300

    added new functioin homework 8

--- homeworks/homework_8/script.js
+++ homeworks/homework_8/script.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const fibonacci = function makeFibonacciFunction(n) {
-    if (n <= 1) {
-        return n;
-    } else {
-        return makeFibonacciFunction(n - 1) + makeFibonacciFunction(n - 2);
-    }
+function makeFibonacciFunction() {
+    let fibPrev = 0;
+    let fibCur = 1;
+    let fibRes = 0;
+    return function getNext() {
+        [fibRes, fibPrev, fibCur] = [fibPrev, fibCur, fibPrev + fibCur];
+        return fibRes;
+    };
+}
+
+let helper = makeFibonacciFunction();
+
+for (let i = 0; i < 10; i++) {
+    console.log(helper());
 }
